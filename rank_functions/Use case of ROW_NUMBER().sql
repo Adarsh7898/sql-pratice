608a56272f88ae5fc1e7c74e020bce7c166e61a6
Adarsh7898 <adarshgoyal220890@acropolis.in>
2026-04-02 12:27:55 +0530

    Added the use case for ROW_NUMBER()

---
+++ rank_functions/Use case of ROW_NUMBER().sql
@@ -0,0 +1,9 @@
+-- Question- Find the products for each productID which have the highest sales
+
+SELECT * FROM (
+SELECT productID, sales, 
+RANK() OVER(PARTITION BY productID ORDER BY sales DESC ) AS product_rank
+FROM orders
+)t1
+WHERE product_rank=1
+ORDER BY sales DESC;
