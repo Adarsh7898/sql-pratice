be618b60bb8d974a5a24c8ddce955c6a247d7187
Adarsh7898 <adarshgoyal220890@acropolis.in>
2026-04-04 12:52:41 +0530

    Added the file on how to Add new column.

--- Extra Functions/COALESCE().sql
+++
@@ -1,9 +0,0 @@
-SELECT name, COALESCE(email,alternateEmail) AS emailAddress
-FROM employees
-WHERE name='Adarsh';
-
-SELECT name, 
--- If the email table is has null then refer to the alternateEmail column and if it finds null in both columns then display Currently Unavailable
-COALESCE(email,alternateEmail,'Currently Unavailable') AS emailAddress
-FROM employees
-WHERE name='Adarsh' OR student_id=122
